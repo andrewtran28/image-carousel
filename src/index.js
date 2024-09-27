e938082c6a2d0932607ddc1fe765ba38fbfd5e48
andrewtran28 <tran.andrew@outlook.com>
2024-09-27 17:45:53 -0400

    Implement image carousel menu

---
+++ src/index.js
@@ -0,0 +1,75 @@
+import "./styles.css";
+
+const menuCarousel = (() => {
+  const IMG_NUM = 5;
+  const IMG_WIDTH = 656; //pixels
+
+  const carousel = document.querySelector(".carousel-frame");
+  const carousel_left = document.querySelector(".carousel-left");
+  const carousel_right = document.querySelector(".carousel-right");
+
+  let index = 0;
+  let imgIndex = [];
+  let btn_nav = [];
+  var autoScroll = setInterval(scrollRight, 5000);
+
+  carousel_left.addEventListener("click", () => {
+    scrollLeft();
+    resetAuto();
+  });
+  carousel_right.addEventListener("click", () => {
+    scrollRight();
+    resetAuto();
+  }); 
+
+  function scrollLeft() {
+    if (index === 0) {
+      index = IMG_NUM - 1;
+      carousel.scrollTo(imgIndex[index], 0);
+      activeNav(index);
+    } else {
+      index--;
+      carousel.scrollTo(imgIndex[index], 0);
+      activeNav(index);
+    }
+  }
+
+  function scrollRight() {
+    if (index === IMG_NUM - 1) {
+      index = 0;
+      carousel.scrollTo(imgIndex[index], 0);
+      activeNav(index);
+    } else {
+      index++;
+      carousel.scrollTo(imgIndex[index], 0);
+      activeNav(index);
+    }
+  }
+
+  function circleNavigation() {
+    for (let i = 0; i < IMG_NUM; i++) {
+      imgIndex[i] = IMG_WIDTH * i;
+      btn_nav[i] = document.getElementById(`nav${i}`);
+      btn_nav[i].addEventListener("click", () => {
+        carousel.scrollTo(imgIndex[i], 0);
+        activeNav(i);
+        index = i;
+        resetAuto();
+      });
+    }
+  }
+
+  function activeNav(index) {
+    btn_nav.forEach((btn) => btn.setAttribute("class", "circle-nav"));
+    btn_nav[index].setAttribute("class", "circle-nav nav-active");
+  }
+
+  function resetAuto(){
+    clearInterval(autoScroll);
+    autoScroll = setInterval(scrollRight, 5000);
+  }
+
+  circleNavigation();
+  activeNav(0);
+  resetAuto();
+})();
